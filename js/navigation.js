ce20f2f0f73e73fdf7faac19f28ff55404856bfd
DonnyJohny <jaximus99@gmail.com>
2017-04-07 11:05:33 +0300

    Сделал фон мобильного меню на всю страницу

--- js/navigation.js
+++ js/navigation.js
@@ -1,30 +1,27 @@
+var navMain = document.querySelector('.main-nav');
+var navUser = document.querySelector('.user-nav');
+var navToggle = document.querySelector('.main-header__toggle');
 
-  var navMain = document.querySelector('.main-nav');
-  var navUser = document.querySelector('.user-nav');
-  var navToggle = document.querySelector('.main-header__toggle');
+navMain.classList.remove('main-nav--nojs');
+navUser.classList.remove('user-nav--nojs');
+navToggle.classList.remove('main-header__toggle--nojs');
 
-  navMain.classList.remove('main-nav--nojs');
-  navUser.classList.remove('user-nav--nojs');
-  navToggle.classList.remove('main-header__toggle--nojs');
-
+if(navToggle){
   navToggle.addEventListener('click', function() {
-    if(navToggle.classList.contains('main-header__toggle--closed')) {
-      if (navMain.classList.contains('main-nav--closed')) {
-        if(navUser.classList.contains('user-nav--closed')) {
-          navMain.classList.remove('main-nav--closed');
-          navUser.classList.remove('user-nav--closed');
-          navToggle.classList.remove('main-header__toggle--closed');
-          navMain.classList.add('main-nav--opened');
-          navUser.classList.add('user-nav--opened');
-          navToggle.classList.add('main-header__toggle--opened');
-        }
-      }
-    } else {
-    navMain.classList.add('main-nav--closed');
-    navUser.classList.add('user-nav--closed');
-    navToggle.classList.add('main-header__toggle--closed');
-    navMain.classList.remove('main-nav--opened');
-    navUser.classList.remove('user-nav--opened');
-    navToggle.classList.remove('main-header__toggle--opened');
+    if (navMain.classList.contains('main-nav--closed') && navUser.classList.contains('user-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navUser.classList.remove('user-nav--closed');
+      navMain.classList.add('main-nav--opened');
+      navUser.classList.add('user-nav--opened');
+      navToggle.classList.add('main-header__toggle--opened');
     }
-  });
+    else {
+      navMain.classList.add('main-nav--closed');
+      navUser.classList.add('user-nav--closed');
+      navToggle.classList.add('main-header__toggle--closed');
+      navMain.classList.remove('main-nav--opened');
+      navUser.classList.remove('user-nav--opened');
+      navToggle.classList.remove('main-header__toggle--opened');
+    }
+  })
+};
